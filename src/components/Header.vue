c71256b1b81c14f962b489fa8a532a3ccfa6c709
Yuri Pratama <dev@ypx.io>
2024-01-21 17:14:33 +0700

    style: header refinement and add input search component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,40 +1,67 @@
 <template>
 	<div id="header-wrapper">
-		<div id="header-text">
-			<h1>Knowledge base doesn't have to be boring</h1>
-			<h3>Everything you need to manage your messaging</h3>
+		<div id="header-container">
+			<div id="header-text">
+				<h1>Knowledge base doesn't have to be boring</h1>
+				<h3>Everything you need to Manage your Messaging</h3>
+			</div>
+			<input-search />
 		</div>
 	</div>
 </template>
 
 <script>
+import InputSearch from './InputSearch.vue';
 export default {
+	components: { InputSearch }
 }
 </script>
 
 <style lang="scss" scoped>
 	@import '../scss/_variables.scss';
-	#header-wrapper {
-		background: white;
-	}
-	#header-text{
-		text-align: center;
-		font-family: $font-family;
-		color: $text-black;
-		padding: 20px;
-
-		h1{
-			font-style: normal;
-			font-weight: bold;
-			font-size: 34px;
-			line-height: 41px;
+	@import '../scss/_mixins.scss';
+	#header {
+		&-wrapper {
+			display: flex;
+			align-items: center;
+			background: white;
+			justify-content: center;
+			padding: 20px;
+			@include breakpoint(sm) {
+				height: 300px;
+			}
 		}
-
-		h3{
-			font-style: normal;
-			font-weight: normal;
-			font-size: 20px;
-			line-height: 24px;
+		&-container {
+			display: flex;
+			flex-direction: column;
+			gap: 37px;
+			width: 100%;
+			@include breakpoint(sm) { width: 480px }
+			@include breakpoint(md) { width: 540px }
+			@include breakpoint(lg) { width: 600px }
+			@include breakpoint(xl) { width: 720px }
+		}
+		&-text {
+			text-align: center;
+			font-family: $font-family;
+			color: $text-black;
+			display: flex;
+			flex-direction: column;
+			gap: 9px;
+			h1, h3 {
+				font-style: normal;
+				margin: 0px;
+			}
+			h1 {
+				font-weight: bold;
+				font-size: 34px;
+				line-height: 41px;
+			}
+			h3 {
+				font-weight: normal;
+				font-size: 20px;
+				line-height: 24px;
+			}
 		}
 	}
 </style>
---
+++ src/components/InputSearch.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="input-search">
+    <input
+      type="text"
+      placeholder="Search for answers"
+    />
+    <button>
+      <i class="fa fa-search fa-lg" />
+    </button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import '../scss/_variables.scss';
+#input-search {
+  width: 100%;
+  display: flex;
+  height: 60px;
+  input {
+    flex-grow: 1;
+    border: 1px solid #eeeeee;
+    border-right: none;
+    padding-left: 22px;
+    border-radius: 4px 0 0 4px;
+    &::placeholder,
+    &::-ms-input-placeholder {
+      color: $text-gray;
+      opacity: 1;
+    }
+    &:hover, &:focus {
+      outline: none;
+    }
+    &:focus {
+      border: 1px solid $green;
+      border-right: none;
+    }
+  }
+  button {
+    background: $green;
+    width: 80px;
+    border: none;
+    border-radius: 0 4px 4px 0;
+    i {
+      color: white;
+    }
+    &:hover {
+      opacity: 0.9;
+      cursor: pointer;
+    }
+  }
+}
+</style>
