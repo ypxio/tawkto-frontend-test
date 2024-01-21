cfb3b4ca73946ef54fb47b2958e4bdce2fa8fd5b
Yuri Pratama <dev@ypx.io>
2024-01-21 20:07:00 +0700

    feat(component): init generic card which can used in home and category view

---
+++ src/components/RegularCard.vue
@@ -0,0 +1,125 @@
+<template>
+  <div
+    v-bind:class="[
+      'card',
+      {
+        'hoverable': $props.hoverable
+      }
+    ]"
+    @click="$emit('click')"
+  >
+    <div class="card__body">
+      
+      <!-- Icon -->
+      <template v-if="$slots.icon">
+        <slot name="icon"></slot>
+      </template>
+      <template v-else>
+        <div class="card__body__icon-slot">
+          <i :class="['fa', 'fa-' + $props.icon, 'fa-3x']" />
+        </div>
+      </template>
+
+      <!-- Title -->
+      <template v-if="$slots.title">
+        <slot name="title"></slot>
+      </template>
+      <template v-else>
+        <h1 class="card__body__title-slot">
+          {{ $props.title }}
+        </h1>
+      </template>
+      
+      <!-- Subtitle -->
+      <template v-if="$slots.subtitle">
+        <slot name="subtitle"></slot>
+      </template>
+      <template v-else>
+        <h4 class="card__body__subtitle-slot">
+          {{ $props.subtitle }}
+        </h4>
+      </template>
+
+    </div>
+    <template v-if="$slots.extra">
+      <div class="card__extra">
+        <slot name="extra"></slot>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['title', 'subtitle', 'icon', 'hoverable'],
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../scss/_mixins.scss';
+@import '../scss/_variables.scss';
+.card {
+  width: 100%;
+  height: 100%;
+  border: 1px solid #eeeeee;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  border-radius: 4px;
+  @include breakpoint(sm) {
+    flex-direction: row;
+  }
+  @include breakpoint(md) {
+    flex-direction: column;
+  }
+  &.hoverable {
+    &:hover {
+      border: 1px solid $green;
+      cursor: pointer;
+    }
+  }
+  &__body {
+    flex: 1;
+    display: flex;
+    padding: 20px 32px;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    &__icon-slot {
+      padding: 20px 0 24px;
+      i {
+        color: $green;
+      }
+    }
+    &__title-slot, &__subtitle-slot {
+      margin: 0;
+    }
+    &__title-slot {
+      font-size: 23px;
+      font-weight: 700;
+      line-height: 28px;
+      text-align: center;
+    }
+    &__subtitle-slot {
+      margin-top: 2px;
+      font-weight: 400;
+      color: $text-gray;
+      font-size: 11px;
+      line-height: 22px;
+    }
+  }
+  &__extra {
+    border-top: 1px solid #eee;
+    @include breakpoint(sm) {
+      flex: 1;
+      border-left: 1px solid #eee;
+      border-top: none;
+    }
+    @include breakpoint(md) {
+      border-left: none;
+      border-top: 1px solid #eee;
+    }
+    padding: 20px 30px;
+  }
+}
+</style>
