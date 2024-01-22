6ba02e0a1e00d676d05428901646f165d70fa546
Yuri Pratama <dev@ypx.io>
2024-01-22 12:26:35 +0700

    chore: add more test samples

--- specs/test.spec.js
+++ specs/test.spec.js
@@ -2,7 +2,7 @@ import { mount } from '@vue/test-utils'
 import RegularCardComponent from '../src/components/RegularCard.vue'
 
 describe('Regular Card', () => {
-  it('default title props', () => {
+  it('show default title component when using props', () => {
     const wrapper = mount(RegularCardComponent, {
       propsData: {
         title: 'Card title'
@@ -12,14 +12,23 @@ describe('Regular Card', () => {
     expect(el.exists()).toBe(true)
     expect(el.text()).toBe('Card title')
   })
-  it('default subtitle props', () => {
+  it('pass component as is when title slot provided', () => {
     const wrapper = mount(RegularCardComponent, {
-      propsData: {
-        subtitle: 'Card subtitle'
+      slots: {
+        title: '<h2 class="title">Custom title</h2>'
       }
     })
-    const el = wrapper.find('h4.card__body__subtitle--default')
+    const el = wrapper.find('h2.title')
     expect(el.exists()).toBe(true)
-    expect(el.text()).toBe('Card subtitle')
+    expect(el.text()).toBe('Custom title')
+  })
+  it('hide default markup component when title slot provided', () => {
+    const wrapper = mount(RegularCardComponent, {
+      slots: {
+        title: '<h3 class="title">Custom title</h3>'
+      }
+    })
+    const el = wrapper.find('h1.card__body__title--default')
+    expect(el.exists()).toBe(false)
   })
 })
